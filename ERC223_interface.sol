fcc1e88757dabd70d3338898d9845f8c31c374f5
jin <junosof@gmail.com>
2021-12-23 22:03:24 +0900

    second commit

--- ERC223_interface.sol
+++ ERC223_interface.sol
@@ -6,4 +6,5 @@ contract ERC223Interface {
     function transfer(address to, uint value);
     function transfer(address to, uint value, bytes data);
     event Transfer(address indexed from, address indexed to, uint value, bytes data);
+    //good check
 }
